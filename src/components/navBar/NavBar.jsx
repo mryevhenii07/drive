2949b102818f98d78a08b3c55b72c9c30918cf0d
mryevhenii07 <mrit.works07@gmail.com>
2022-08-21 14:52:36 +0300

    create navBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { AiOutlineMenu, AiOutlineClose, AiOutlineSearch, AioutlinePerson } from 'react-icons/ai';
+import React from "react";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiOutlineSearch,
+  AioutlinePerson,
+} from "react-icons/ai";
 
-import s from './NavBar.module.css';
-import Logo from '../../images/logo.png';
+import s from "./NavBar.module.css";
+import Logo from "../../images/logo.png";
 
 const NavBar = () => {
   return (
     <header className={s.navBar}>
       <img src={Logo} alt="/" />
+      <ul className={s.menu}>
+        <li>
+          <a href="/">Learn More</a>
+        </li>
+        <li>
+          <a href="/">Log in</a>
+        </li>
+        <li>
+          <a href="/">Sing up</a>
+        </li>
+      </ul>
     </header>
   );
 };
