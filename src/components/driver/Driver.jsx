9c846c0e0e493a6c8c3593a2396ac51f6198adc5
mryevhenii07 <mrit.works07@gmail.com>
2022-08-21 21:42:51 +0300

    finished driver

--- src/components/driver/Driver.jsx
+++ src/components/driver/Driver.jsx
@@ -9,6 +9,16 @@ const Driver = () => {
       <div className={s.left}>
         <img src={Drive} alt="/" />
       </div>
+      <div className={s.right}>
+        <h2>
+          Find your perfect car <span>to try before you buy</span>
+        </h2>
+        <p>
+          Make sure your future wheels work well with your lifestyle by taking
+          your time in the drivers seat.
+        </p>
+        <button>Browser Cars</button>
+      </div>
     </div>
   );
 };
---
+++ src/components/luxury/Luxury.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import s from "./Luxury.module.css";
+
+const Luxury = () => {
+  return <div className={s.luxury}>Luxury</div>;
+};
+
+export default Luxury;
