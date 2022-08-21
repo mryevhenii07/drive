b41d9377337826de0c1c2a88fd5d85a059395723
mryevhenii07 <mrit.works07@gmail.com>
2022-08-21 18:31:34 +0300

    finished form block

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import NavBar from './components/navBar/NavBar';
+import NavBar from "./components/navBar/NavBar";
+import Hero from "./components/hero/Hero";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 }
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+
+import s from "./Hero.module.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className={s.hero}>
+      <form action="">
+        <div className={s.text}>
+          <label htmlFor="">Where</label>
+          <input
+            className={s.textInput}
+            type="text"
+            placeholder="Search Location"
+          />
+        </div>
+        <div className={s.from}>
+          <span className={s.border}></span>
+          <label htmlFor="">From</label>
+          <input type="date" />
+        </div>
+        <div className={s.until}>
+          <label htmlFor="">Until</label>
+          <input type="date" />
+        </div>
+        <div className={s.searchBtn}>
+          <AiOutlineSearch className={s.icon} />
+          <button className={s.btn}>Search for cars</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Hero;
